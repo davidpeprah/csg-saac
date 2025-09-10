81ae8c15486b29d7876e80e7ac7969383f87223a
davidpeprah <peprahdavid@gmail.com>
2025-09-09 22:51:40 -0400

    updated RUN command

--- RUN.cmd
+++ RUN.cmd
@@ -1,3 +1,3 @@
 @echo off
 REM This script is used to run the SAAC application
-"C:\Program Files\Python313\python.exe" C:\csg-saac\Main.py
+"C:\Program Files\Python313\python.exe" c:\scripts\csg-saac\Main.py
